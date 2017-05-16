2da462769a263c8b9ec6155b4b55702f2218a685
William Schokkelé <william.schokkele@howest.be>
2017-05-16 14:31:30 +0200

    Add project files.

---
+++ GitMonitoraat_TeamLeader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitMonitoraat_TeamLeader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
